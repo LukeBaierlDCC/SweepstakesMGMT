3d08fcc429e7d6763f769839c66e2a19be034cee
Luke <lukeest1984@gmail.com>
2019-02-15 08:25:51 -0600

    Initial commit.

--- SweepstakesMGMT/Program.cs
+++ SweepstakesMGMT/Program.cs
@@ -10,8 +10,6 @@ namespace SweepstakesMGMT
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 

874833d02f69b08d13327eefc0cedb857064ea2f
Luke <lukeest1984@gmail.com>
2019-02-18 15:21:16 -0600

    Debugged interface queue manager error; however, exploring alternatives that require less code.

--- SweepstakesMGMT/SweepstakesQueueManager.cs
+++ SweepstakesMGMT/SweepstakesQueueManager.cs
@@ -7,5 +7,20 @@ namespace SweepstakesMGMT
 {
     public class SweepstakesQueueManager : ISweepstakesManager
     {
+
+        Sweepstakes ISweepstakesManager.GetSweepstakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISweepstakesManager.InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISweepstakesManager.InsertSweepstakes(object p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
